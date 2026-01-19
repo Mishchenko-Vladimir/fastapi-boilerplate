24c43d4e9f33e0c4bb0c89fcf15052bfd67fe7b6
Владимир <mishchienko.2001@mail.ru>
2026-01-19 19:21:16 +0300

    Init auth dependencies for api

--- app/api/dependencies/current_user.py
+++ app/api/dependencies/current_user.py
@@ -1,14 +1,11 @@
 __all__ = (
-    "get_db",
     "optional_user",
     "current_active_user",
     "current_active_verified_user",
     "current_active_superuser",
 )
 
-
-from .get_db import get_db
-from .current_user import (
+from core.auth.dependencies.fastapi_users import (
     optional_user,
     current_active_user,
     current_active_verified_user,

f156e0f281703acb05df766aeef42ac6e4b6716f
Владимир <mishchienko.2001@mail.ru>
2026-01-21 22:17:35 +0300

    Add settings, db_helper, limiter, templates in init

---
+++ app/core/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    "settings",
+    "db_helper",
+    "limiter",
+    "templates",
+)
+
+from .config import settings
+from .db_helper import db_helper
+from .limiter import limiter
+from .templates import templates

f8f870caf4ece6f66ac11b3a896018f6d7776f7a
Владимир <mishchienko.2001@mail.ru>
2026-01-22 13:39:50 +0300

    Add BASE DIR in init

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,11 +1,12 @@
 __all__ = (
     "settings",
+    "BASE_DIR",
     "db_helper",
     "limiter",
     "templates",
 )
 
-from .config import settings
+from .config import settings, BASE_DIR
 from .db_helper import db_helper
 from .limiter import limiter
 from .templates import templates
